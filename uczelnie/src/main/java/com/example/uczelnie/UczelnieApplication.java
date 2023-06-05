a6d0508b913dbc2e00692585824ca3b2d941e63d
kyzcaktajam <92443047+kyzcaktajam@users.noreply.github.com>
2023-06-05 13:41:40 +0200

    Add files via upload

---
+++ uczelnie/src/main/java/com/example/uczelnie/UczelnieApplication.java
@@ -0,0 +1,13 @@
+package com.example.uczelnie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UczelnieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UczelnieApplication.class, args);
+    }
+
+}
---
+++ uczelnie/src/test/java/com/example/uczelnie/UczelnieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uczelnie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UczelnieApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
