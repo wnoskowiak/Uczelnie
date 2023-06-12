bd7ed2e02e027f8f870be18e462c5e3ca9a161da
Wojciech <wojtek.noskowiak@gmail.com>
2023-06-12 20:17:26 +0200

    update 3

--- uczelnie/src/main/java/com/example/uczelnie/login/repository/RoleRepository.java
+++ uczelnie/src/main/java/com/example/uczelnie/login/repository/RoleRepository.java
@@ -7,8 +7,66 @@ import org.springframework.stereotype.Repository;
 import com.example.uczelnie.login.models.ERole;
 import com.example.uczelnie.login.models.Role;
 
+// @Repository
+// public interface RoleRepository extends JpaRepository<Role, Long> {
+//   Optional<Role> findByName(ERole name);
+// }
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.example.uczelnie.login.repository.ObjectRepository;
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, Long> {
-  Optional<Role> findByName(ERole name);
+public class RoleRepository implements ObjectRepository<Role> {
+
+  private Map<Long, Role> repository;
+
+  public RoleRepository() {
+    this.repository = new HashMap<>();
+  }
+
+  @Override
+  public void store(Role emp) {
+    repository.put(emp.getId(), emp);
+  }
+
+  @Override
+  public Role retrieve(Long id) {
+    return repository.get(id);
+  }
+
+  public Optional<Role> findByName(ERole name) {
+    Collection<Role> emps = repository.values();
+    for (Role emp : emps) {
+      if (emp.getName()==name)
+        return Optional.of(emp);
+    }
+    return Optional.empty();
+  }
+
+  @Override
+  public Optional<Role> search(String name) {
+    return Optional.empty();
+  }
+
+  @Override
+  public Role delete(Long id) {
+    Role e = repository.get(id);
+    this.repository.remove(id);
+    return e;
+  }
+
+  public boolean existsByUsername(String username) {
+    if (this.search(username).isEmpty()) {
+      return false;
+    }
+    return true;
+  }
+
+  public boolean existsByEmail(String email) {
+    return false;
+  }
+
 }
