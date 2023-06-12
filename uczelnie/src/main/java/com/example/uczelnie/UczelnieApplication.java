d92ad7cc4f5f4f0ddbe683cc130c839ee74c604d
jszot <j.szot@student.uw.edu.pl>
2023-06-12 20:12:39 +0200

    usilne próby uruchomienia aplikacji

--- uczelnie/src/main/java/com/example/uczelnie/UczelnieApplication.java
+++ uczelnie/src/main/java/com/example/uczelnie/UczelnieApplication.java
@@ -2,8 +2,9 @@ package com.example.uczelnie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class UczelnieApplication {
 
     public static void main(String[] args) {
--- uczelnie/src/main/java/com/example/uczelnie/login/security/jwt/AuthTokenFilter.java
+++ uczelnie/src/main/java/com/example/uczelnie/login/security/jwt/AuthTokenFilter.java
@@ -17,8 +17,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+//import com.example.uczelnie.login.security.jwt.JwtUtils;
 
 public class AuthTokenFilter extends OncePerRequestFilter {
+
     @Autowired
     private JwtUtils jwtUtils;
 
